9f0a160390347d273c415540c07afafa053746f3
LIYUNGANG <liyungang881003@gmail.com>
2024-07-27 11:15:21 +0800

    Initial Commit

---
+++ 0727/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  0727
+//
+//  Created by Clovis Lee on 2024/7/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 0727/_727App.swift
@@ -0,0 +1,17 @@
+//
+//  _727App.swift
+//  0727
+//
+//  Created by Clovis Lee on 2024/7/27.
+//
+
+import SwiftUI
+
+@main
+struct _727App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
